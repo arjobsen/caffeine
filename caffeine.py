61db97ebb80071947800b440b92be30cfa65e8d5
Arjen Bos <abos@eiffel.nl>
2021-02-12 07:52:27 +0100

    Commit van EIFFEL laptop

--- caffeine.py
+++ caffeine.py
@@ -1,4 +1,25 @@
 import pyautogui
+import time
+import sys
 
+pyautogui.FAILSAFE = False
 
-pyautogui.moveRel(50,50, duration=1)
+# Get interval (seconds) from CLI argument
+if len(sys.argv) > 1:
+    interval = float(sys.argv[1])
+else:
+    interval = 60
+print("Interval set:", str(interval), "sec")
+
+i = 0
+print("Break with Ctrl+C")
+while True:
+    #pyautogui.moveTo(100, 100, duration=1)
+    #pyautogui.moveRel(0, 50, duration=1)
+    #pyautogui.press('volumeup')
+    time.sleep(1)
+    #pyautogui.press('volumedown')
+    pyautogui.press('shift')
+    time.sleep(interval)
+    i -= -1
+    print("\r" + str(i), end='')
